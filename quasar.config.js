3cacc1149d13a76e19f56f65901123bf686eae38
Chris Louis <chrislouis@Chriss-MacBook-Air.local>
2025-11-02 22:39:45 +0100

    google analytics

--- quasar.config.js
+++ quasar.config.js
@@ -8,95 +8,87 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
-const { configure } = require('quasar/wrappers')
-const path = require('path')
+const { configure } = require("quasar/wrappers");
+const path = require("path");
 
 module.exports = configure(function (/* ctx */) {
   return {
     eslint: {
       warnings: true,
-      errors: true
+      errors: true,
     },
 
-    css: [
-      'app.scss'
-    ],
+    css: ["app.scss"],
 
     extras: [
-      'roboto-font',
-      'material-icons',
-        'fontawesome-v6' // ✅ this loads Font Awesome officially into Quasar
+      "roboto-font",
+      "material-icons",
+      "fontawesome-v6", // ✅ this loads Font Awesome officially into Quasar
     ],
 
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
-        node: 'node16'
+        browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
+        node: "node16",
       },
 
-      vueRouterMode: 'history',
+      vueRouterMode: "history",
 
-      extendViteConf (viteConf) {
-        viteConf.resolve = viteConf.resolve || {}
+      extendViteConf(viteConf) {
+        viteConf.resolve = viteConf.resolve || {};
         viteConf.resolve.alias = {
           ...(viteConf.resolve.alias || {}),
-          layouts: path.resolve(__dirname, 'src/layouts'),
-          pages: path.resolve(__dirname, 'src/pages'),
-          components: path.resolve(__dirname, 'src/components')
-        }
-      }
+          layouts: path.resolve(__dirname, "src/layouts"),
+          pages: path.resolve(__dirname, "src/pages"),
+          components: path.resolve(__dirname, "src/components"),
+        };
+      },
     },
 
     devServer: {
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
     },
 
     framework: {
       config: {},
 
-  plugins: []
-},
+      plugins: [],
+    },
 
-
-
-    animations: 'all',
+    animations: "all",
 
     ssr: {
       pwa: false,
       prodPort: 3000,
-      middlewares: [
-        'render'
-      ]
+      middlewares: ["render"],
     },
 
     pwa: {
-      workboxMode: 'generateSW',
+      workboxMode: "generateSW",
       injectPwaMetaTags: true,
-      swFilename: 'sw.js',
-      manifestFilename: 'manifest.json',
+      swFilename: "sw.js",
+      manifestFilename: "manifest.json",
       useCredentialsForManifestTag: false,
     },
 
     cordova: {},
 
     capacitor: {
-      hideSplashscreen: true
+      hideSplashscreen: true,
     },
 
     electron: {
       inspectPort: 5858,
-      bundler: 'packager',
+      bundler: "packager",
       packager: {},
       builder: {
-        appId: 'tianna'
-      }
+        appId: "tianna",
+      },
     },
 
     bex: {
-      contentScripts: [
-        'my-content-script'
-      ],
-      boot: ['firebase']
-    }
-  }
-})
+      contentScripts: ["my-content-script"],
+      boot: ["firebase", "google-analytics"],
+    },
+  };
+});
