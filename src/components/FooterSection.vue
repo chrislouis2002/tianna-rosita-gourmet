468d6df25f315cd423f3dc5e119783fea7d2e50a
Chris Louis <chrislouis2002@gmail.com>
2026-08-26 12:48:48 +0100

    test new deployment testflow

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -9,7 +9,7 @@
       <!-- Contact Info -->
       <div class="footer-section">
           <p class="text-h6 text-weight-medium text-yellow-6 q-my-lg">VISIT US</p>
-        <p>📍 Solution Fun City, Awka, Anambra</p>
+        <p>📍 Solution Fun City, Awka, Anambra State</p>
         <p>📞 +234 706 963 7571</p>
         <p>📞 +234 812 969 1245</p>
         <p>📩 tiannarosita@gmail.com</p>
